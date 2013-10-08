2009aa1cec5233aa1b827f22cbb16d1d00619ea4
Martin Henry <emailnitram@gmail.com>
2013-10-07 18:20:49 -0700

    added basic map using leaflet-directive

--- www/js/controllers/map.js
+++ www/js/controllers/map.js
@@ -1,9 +1,9 @@
 angular.module('meetMeApp.controller.map', [])
   .controller('MapCtrl', ['$scope', function ($scope) {
-    $scope.map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('http://{s}.tile.cloudmade.com/ea2043c6155f431c939a095e5bf7d521/997/256/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-      maxZoom: 18
-    }).addTo(map);
+    // $scope.map = L.map('map').setView([51.505, -0.09], 13);
+    // L.tileLayer('http://{s}.tile.cloudmade.com/ea2043c6155f431c939a095e5bf7d521/997/256/{z}/{x}/{y}.png', {
+    //   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    //   maxZoom: 18
+    // }).addTo(map);
   }]
 );
