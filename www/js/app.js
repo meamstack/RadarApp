f5602c514f52a3164fb8406efc4338da822cf967
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-10-09 20:11:03 -0700

    Facebook login flow implemented, and angular mobile nav implemented

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,10 @@
 var iPhoneApp = angular.module('meetMeApp', [
   'meetMeApp.controller.main',
   'meetMeApp.controller.map',
+  'ajoslin.mobile-navigate',
   'meetMeApp.controller.createActivity',
+  'meetMeApp.controller.viewActivity',
+  'meetMeApp.controller.chat',
   'leaflet-directive',
   'meetMeApp.directive.createActivity'
 ]);
--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -1,46 +1,21 @@
 angular.module('meetMeApp.controller.main', [])
-  .controller('MainCtrl', ['$scope', '$location', function ($scope, $location) {
+  .controller('MainCtrl', ['$scope', '$location', '$navigate', '$timeout', function ($scope, $location, $navigate, $timeout) {
 
+    $scope.$navigate = $navigate;
     $scope.login = function() {
       var ref = window.open('http://meetme123.com:3000/auth/facebook/', '_blank', 'location=no,toolbar=no');
       ref.addEventListener('loadstart', function(event){
         if (event.url === 'http://meetme123.com:3000/') {
           ref.close();
-          $scope.changepage();
+          $timeout(function(){
+            $scope.$navigate.go('/map', 'swipe');
+          }, 0);
         }
       });
     };
 
-    $scope.changepage = function() {
-      location.assign($location.path('/map'));
+    $scope.clickdemo = function(){
+      $scope.$navigate.go('/map', 'slide');
     };
-
-
-
-
-
-
-
-
-      // $scope.onGeoSuccess = function(position) {
-      //   $scope.latitude = position.coords.latitude;
-      //   $scope.longitude = position.coords.longitude;
-      //   $scope.altitude = position.coords.altitude;
-      //   $scope.accuracy = position.coords.accuracy;
-      //   $scope.altitudeAccuracy = position.coords.altitudeAccuracy;
-      //   $scope.heading = position.coords.heading;
-      //   $scope.speed = position.coords.speed;
-      //   $scope.timestamp = position.timestamp;
-      // };
-
-      // // onError Callback receives a PositionError object
-      // //
-      // $scope.onError = function(error) {
-      //     alert('code: '    + error.code    + '\n' +
-      //           'message: ' + error.message + '\n');
-      // };
-      // $scope.runLocate = function() {
-      //   navigator.geolocation.getCurrentPosition($scope.onGeoSuccess, $scope.onError);
-      // };
 
   }]);
