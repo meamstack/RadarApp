2f96e84cc5767cfd40d56e014eff5820a9ffcc55
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-10-10 20:20:31 -0700

    hammer tap works

--- www/js/controllers/map.js
+++ www/js/controllers/map.js
@@ -1,10 +1,22 @@
 angular.module('meetMeApp.controller.map', [])
   .controller('MapCtrl', ['$scope', 'googleMapInit', function ($scope, googleMapInit) {
-    console.log('its working')
     $scope.addMarker = function() {
       googleMapInit.addMarker(map,37.785427,-122.40572, "Hi")
     }
     googleMapInit.initializeGoogleMap();
-  }]
-);
+    $scope.slide = function () {
+      var element = document.getElementById('hourSlider');
+      // alert('test');
+      // element.addEventListener('swipedown', function (event) {
+      //   if (event.targetTouches.length === 1) 
+      //     var touch = event.targetTouches[0];
+      //     alert('touched');
+      // });
+      var hour = 1;
+      var hammertime = Hammer(element).on("swipeup", function(event, hour) {
+        hour++;
+    });
+
+  };
+}]);
 
