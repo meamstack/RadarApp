1b6e96b4de491e158b7fd9806a2fc4edab6ed35f
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-10-11 22:00:51 -0700

    added animation to the pin. Corrected spelling on map style

--- www/js/services/google-map-factory.js
+++ www/js/services/google-map-factory.js
@@ -60,7 +60,8 @@ googleMapFactory.factory('googleMapInit', function () {
       var markerLatlng = new google.maps.LatLng(lat,long);
       var marker = new google.maps.Marker({
           position: markerLatlng, 
-          map: map, 
+          map: map,
+          animation: google.maps.Animation.DROP,
           title:"San Francisco Ferry Building"});
       // icon: "http://library.csun.edu/images/google_maps/marker-blue.png"});   
     google.maps.event.addListener(marker, 'click', function() {
