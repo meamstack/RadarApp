199026edd9bb3983e8ddf08ae60329b596fac3f1
Hack Reactor Students <github@hackreactor.com>
2013-10-12 19:17:01 -0700

    createActivity page improvements, added user to activity. WIP

--- www/js/services/userData.js
+++ www/js/services/userData.js
@@ -3,21 +3,23 @@ angular.module("meetMeApp.service.userData", [])
 
     var user;
     var init = function() {
-	    var deferred = $q.defer();
-	    
-	    // CHANGE URL WHEN DEPLOYED TO FINAL SERVER
-	    $http.get('http://meetme123.com:3000/api/getUserData')  // getting all user data
-	    .success(function(userData, status, headers, config) {
-	    	user = userData;
-	      deferred.resolve(userData);
-	    }).error(function(err, status, headers, config) {if(err) throw err;});
-	    return deferred.promise;
-	  };
+      var deferred = $q.defer();
 
-	  return {
-	    init: init,
-	    getUser: function(){
-	    	return user;
-	    }
-	  };
+      // CHANGE URL WHEN DEPLOYED TO FINAL SERVER
+      $http.get('http://edhsieh.com/api/getUserData')  // getting all user data
+      .success(function(userData, status, headers, config) {
+        user = userData;
+        deferred.resolve(userData);
+      }).error(function(err, status, headers, config) {if(err) throw err;});
+      return deferred.promise;
+    };
+
+    return {
+      init: init,
+      getUser: function(){
+        // alert('user',user);
+        // console.log('user',user);
+        return user;
+      }
+    };
   }]);
