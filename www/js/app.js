5b1e80d18a3f0a90fe4ba3eecdad5bde4857f546
Martin Henry <emailnitram@gmail.com>
2013-10-17 17:37:28 -0700

    updated to reflect removal of dependencies

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,13 @@
 var iPhoneApp = angular.module('meetMeApp', [
   'meetMeApp.controller.main',
   'meetMeApp.controller.map',
-  'meetMeApp.controller.chat',
   'meetMeApp.service.eventLocation',
   'meetMeApp.service.postToServer',
   'ajoslin.mobile-navigate',
   'meetMeApp.controller.createActivity',
-  'meetMeApp.controller.viewActivity',
   'meetMeApp.directive.createActivity',
   'google-map-service',
   'meetMeApp.service.userData',
+  'meetMeApp.service.loginCheck',
   'hammer'
 ]);
